5c72ea3000c677e47f2048dcbb3f6349d3bb7405
lijingbo8119 <admin@lijingbo.cn>
2018-11-06 17:04:47 +0800

    server set

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-echo 1122333;
 define('LARAVEL_START', microtime(true));
 /*
 |--------------------------------------------------------------------------
